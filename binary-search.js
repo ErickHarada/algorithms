7a8938993e20df5a6d9f7a3b57a10e02763dc847
Erick <eefeaga2000@gmail.com>
2024-01-25 20:27:54 -0300

    binary search

---
+++ binary-search.js
@@ -0,0 +1,20 @@
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let start = 0;
+let end = list.length - 1;
+const findNumber = 1;
+
+
+while (start <= end) {
+    let middle = Math.floor((start + end) / 2); //Math.floor() always round down the number
+    let guess = list[middle];
+
+    if (guess === findNumber) {
+        return console.log("numero", guess);
+    } else if (guess > findNumber) {
+        end = middle - 1;
+    } else {
+        start = middle + 1;
+    }
+}
+
